36a1260f4c0c86515739c41cf8a9e3a7f3b3e8fd
Sema <sema@pyva.net>
2018-11-02 23:19:19 -0400

    Added polymer folder

---
+++ polymer/index.js
@@ -0,0 +1,7 @@
+import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import Lcd from "char-lcd";
+
+export class CharLcd extends PolymerElement {
+}
+
+customElements.define('char-lcd', CharLcd);
