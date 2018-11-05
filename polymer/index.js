b89c53369d502080e38ab53d0370264411ce7512
Sema <sema@pyva.net>
2018-11-04 23:59:49 -0500

    Polymer - in progress

--- polymer/index.js
+++ polymer/index.js
@@ -1,7 +1,32 @@
 import {PolymerElement} from '@polymer/polymer/polymer-element.js';
+import {html} from '@polymer/polymer/polymer-legacy.js';
 import Lcd from "char-lcd";
 
+console.log('CharLcd', window.CharLcd);
+
 export class CharLcd extends PolymerElement {
+
+  constructor() {
+    super();
+  }
+
+  static get template() {
+    return html``;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+console.log('CharLcd - connectedCallback()', this.shadowRoot, Lcd);
+this.shadowRoot.innerHTML = 'YES IT WORKS!';
+
+//this lcd = new window.CharLcd({ at: this.shadowRoot });
+  }
+
+  ready() {
+    super.ready();
+console.log('CharLcd - ready()');
+  }
+
 }
 
 customElements.define('char-lcd', CharLcd);
